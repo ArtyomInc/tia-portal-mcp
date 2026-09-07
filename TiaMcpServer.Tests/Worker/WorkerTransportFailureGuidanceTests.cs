d4d5e8f1ce35193c1fdc8ee9ccb3387d37b7749d
LCZ <czarnacki.lukasz.rafal@gmail.com>
2026-09-07 15:56:33 +0200

    test(worker): define transport guidance classification

---
+++ TiaMcpServer.Tests/Worker/WorkerTransportFailureGuidanceTests.cs
@@ -0,0 +1,22 @@
+using TiaMcpServer.Worker;
+using Xunit;
+
+namespace TiaMcpServer.Tests.Worker;
+
+public sealed class WorkerTransportFailureGuidanceTests
+{
+    [Theory]
+    [InlineData("browse_project_tree", true)]
+    [InlineData("browse_project_tree_v3_snapshot", true)]
+    [InlineData("get_block_content", true)]
+    [InlineData("read_update_tag_safety_snapshot", true)]
+    [InlineData("compile_check", false)]
+    [InlineData("save_project", false)]
+    [InlineData("update_tag", false)]
+    [InlineData("start_plc", false)]
+    [InlineData("unknown_method", false)]
+    [InlineData("", false)]
+    [InlineData(null, false)]
+    public void SafetyClassificationComesFromOperationPolicy(string? method, bool expectedSafe)
+        => Assert.Equal(expectedSafe, WorkerTransportFailureGuidance.IsSafeRead(method));
+}
